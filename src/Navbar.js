2e73255c2179df72ef30f01ab88dd9e2cde794cd
Tang <ricenoddlet@gmail.com>
2019-12-05 14:44:25 -0600

    Add homepage route
    Add PaletteList for homepage

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
 import Slider from 'rc-slider';
 import Select from '@material-ui/core/Select';
 import Snackbar from '@material-ui/core/Snackbar';
@@ -39,7 +40,7 @@ class Navbar extends Component {
         return(
             <header className="Navbar">
                 <div className="logo">
-                    <a href="/">MassiveColorPicker</a>
+                    <Link to="/">MassiveColorPicker</Link>
                 </div>
                 <div className="slider-container">
                     <span>Level: {level}</span>
