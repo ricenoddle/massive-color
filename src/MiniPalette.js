97b85a3c011f7cbfc029f028f503ef34eb35e47b
Tang <ricenoddlet@gmail.com>
2019-12-11 13:29:11 -0600

    Refactor with PureComponent

--- src/MiniPalette.js
+++ src/MiniPalette.js
@@ -1,26 +1,30 @@
-import React, {Component} from 'react';
+import React, {PureComponent} from 'react';
 import styles from './styles/MiniPaletteStyle';
 import {withStyles} from '@material-ui/styles';
 import DeleteIcon from '@material-ui/icons/Delete';
 
-class MiniPalette extends Component{
+class MiniPalette extends PureComponent{
     constructor(props){
         super(props);
         this.deletePalette = this.deletePalette.bind(this);
+        this.handleClick = this.handleClick.bind(this);
     }
 
     deletePalette(e){
         e.stopPropagation();
         this.props.openDialog(this.props.id);
     }
+    handleClick(){
+        this.props.goToPalette(this.props.id);
+    }
 
     render(){
-        const {classes, paletteName, emoji, colors, handleClick} = this.props;
+        const {classes, paletteName, emoji, colors, handleClick, id} = this.props;
         const miniColorBoxes = colors.map(color => (
             <div key={color.name} className={classes.miniColor} style={{backgroundColor: color.color}}></div>
         ));
         return (
-            <div className={classes.root} onClick={handleClick}>
+            <div className={classes.root} onClick={this.handleClick}>
                 <DeleteIcon className={classes.deleteIcon} onClick={this.deletePalette}/>
                 <div className={classes.colors}>
                     {miniColorBoxes}
--- src/PaletteList.js
+++ src/PaletteList.js
@@ -26,6 +26,7 @@ class PaletteList extends Component {
         this.openDialog = this.openDialog.bind(this);
         this.closeDialog = this.closeDialog.bind(this);
         this.handleDelete = this.handleDelete.bind(this);
+        this.goToPalette = this.goToPalette.bind(this);
     }
 
     openDialog(id){
@@ -63,7 +64,7 @@ class PaletteList extends Component {
                                 <MiniPalette key={palette.id} 
                                              id={palette.id} 
                                              {...palette} 
-                                             handleClick={() => this.goToPalette(palette.id)} 
+                                             goToPalette={this.goToPalette} 
                                              openDialog={this.openDialog}/>
                             </CSSTransition>
                         ))}
